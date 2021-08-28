8d3af82ca7402898a8b2f2cc544905ac536364cc
Forrest Feaser <34611144+fdfea@users.noreply.github.com>
2021-08-27 22:25:41 -0400

    Update fklog.c

--- fklog.c
+++ fklog.c
@@ -1,12 +1,12 @@
 #include <getopt.h>
 #include <signal.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-#include <linux/input-event-codes.h>
 #include <linux/input.h>
+#include <linux/input-event-codes.h>
 
 #define AND     &&
 #define OR      ||
